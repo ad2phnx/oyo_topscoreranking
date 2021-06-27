72b45f27a18081317ac258a6421431cc7c2b5ac7
Dinis, Anatoliy | DSAID <ts-anatoliy.dinis@rakuten.com>
2021-06-27 04:07:45 +0000

    Passed first tests and added controllers.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ApplicationRecord
+    # association
+    has_many :scores, dependent: :destroy
+
+    # validations
+    validates_presence_of :name
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,7 @@
 class Score < ApplicationRecord
+  # association
   belongs_to :player
+
+  # validation
+  validates_presence_of :score, :time
 end
